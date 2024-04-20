be03d6d9fef6f4bcd79028df646d8b9670cd5766
Abderezzak22 <107290647+Abderezzak22@users.noreply.github.com>
2024-04-20 16:55:32 +0100

    Update presentation_layer.py

--- presentation_layer.py
+++ presentation_layer.py
@@ -2,34 +2,46 @@ import os
 import telebot
 from domain_layer import get_response
 
+# Load Telegram API Key from environment variable
 TELEGRAM_API_KEY = os.getenv('TELEGRAM_API_KEY')
+
+# Create a new Telegram bot instance
 bot = telebot.TeleBot(TELEGRAM_API_KEY)
 
-
+# Handler for /start and /help commands
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-    help_text = """Welcome to the Gemini AI Bot! 🤖
-    
-This bot is integrated with Gemini AI. You can ask it questions and it will provide answers based on its AI capabilities.
+    help_text = """مرحبًا بك في 𝐓𝐑𝐎𝐔𝐘 🤖
 
-Here's how to use it:
+تم دمج هذا الروبوت مع الذكاء الاصطناعي. يمكنك طرح الأسئلة عليه وسيقدم لك الإجابات بناءً على قدرات الذكاء الاصطناعي الخاصة به.
 
-1. Simply type your question in the chat and send it.
-2. The bot will process your question and provide an answer as soon as possible.
+وإليك كيفية استخدامه:
 
-Please note that the quality of the answer depends on the complexity of the question. The bot is designed to handle a wide range of topics, but it might not be able to answer very specific or complex questions.
+1. ما عليك سوى كتابة سؤالك في الدردشة وإرساله.
 
-Enjoy using the Gemini AI Bot!
-    """
+2. سيقوم الروبوت بمعالجة سؤالك وتقديم إجابة في أقرب وقت ممكن.
+
+يرجى ملاحظة أن جودة الإجابة تعتمد على مدى تعقيد السؤال. تم تصميم الروبوت للتعامل مع مجموعة واسعة من المواضيع، لكنه قد لا يتمكن من الإجابة على أسئلة محددة أو معقدة للغاية.
+
+تم تطويري من قبل: @AY_Y4
+
+استمتع باستخدامي 🥰♥
+
+"""
     bot.reply_to(message, help_text)
 
-
+# Handler for all other messages
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
-    wait_message = bot.reply_to(message, "Please wait... 🕐")
+    wait_message = bot.reply_to(message, "ستنى نشوف..🕐")
     response = get_response(message.text)
     bot.delete_message(wait_message.chat.id, wait_message.message_id)
     bot.reply_to(message, response.text)
 
+# Start the bot polling
+def start():
+    bot.infinity_polling()
 
-bot.infinity_polling()
+# Run the bot if this file is executed
+if __name__ == "__main__":
+    start()
